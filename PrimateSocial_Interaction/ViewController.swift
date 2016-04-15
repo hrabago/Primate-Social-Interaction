5497bbcb87ebe7c322ca7a78e181cba9290fc982
Héctor Rábago <Hector@hectors-macbook-pro.local>
2016-04-15 01:24:48 -0400

    Initial Commit

---
+++ PrimateSocial_Interaction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimateSocial_Interaction
+//
+//  Created by Héctor Rábago on 4/15/16.
+//  Copyright © 2016 Héctor Rábago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
